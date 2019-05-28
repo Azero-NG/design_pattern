f8503ec98296b50d2035140ac2cdd0ad532c3296
Azero <perfect@bjtu.edu.cn>
2019-05-28 09:50:18 +0800

    单例模式

---
+++ src/main/java/singleton/Singleton.java
@@ -0,0 +1,8 @@
+public class Singleton{
+    private static Singleton instance = new Singleton();
+    private Singleton(){}
+
+    public static Singleton newInstance(){
+        return instance;
+    }
+}
